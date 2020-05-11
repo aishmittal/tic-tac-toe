69d220761f48031b320abb777116514e897477e7
Aishwarya Mittal <aimittal@adobe.com>
2020-05-11 14:41:22 +0530

    Updates

---
+++ precache-manifest.299df021daeec45c67949dbbb244f440.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1027c35dd67b4c3bba88dd57bb377a6a",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "62433fce70cca14fe2e3",
+    "url": "/tic-tac-toe/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "a21d82df8e6e19a064d6",
+    "url": "/tic-tac-toe/static/js/2.6873c516.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.6873c516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62433fce70cca14fe2e3",
+    "url": "/tic-tac-toe/static/js/main.accbc1ad.chunk.js"
+  },
+  {
+    "revision": "67093bcd19aa82d2fa67",
+    "url": "/tic-tac-toe/static/js/runtime-main.29e8e2be.js"
+  }
+]);
